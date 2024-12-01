e48649df39d798637ddc2b64cd488a3c1cd43f75
It'sMe <164509828+Kolineee@users.noreply.github.com>
2024-12-01 09:19:37 -0800

    Add files via upload

---
+++ borrow_admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Selection</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <div class="container-index">
+        <h1>Login as</h1>
+        <div class="login-selection">
+            <a href="admin_login.php" class="login-btn">Admin</a>
+            <a href="sub_admin_login.php" class="login-btn">Sub-Admin</a>
+        </div>
+    </div>
+</body>
+</html>
